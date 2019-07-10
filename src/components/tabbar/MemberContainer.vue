7976f6d0dfa0c44e1666e4bc412b54bc944b9bb0
LiCh3ng <justdoitgogogo@163.com>
2019-07-10 16:54:27 +0800

    first 修改

---
+++ src/components/tabbar/MemberContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>MemberContainer</h3>
+  </div>
+</template>
+
+<script>
+import { Toast } from "mint-ui";
+export default {
+  created() {
+    Toast({
+      message: "对不起, 请求服务器失败",
+      iconClass: "icon icon-success"
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
